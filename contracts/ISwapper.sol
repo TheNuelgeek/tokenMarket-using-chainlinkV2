38d14c03e98111824f2691b5209a754b1d82f324
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-03-19 19:03:05 +0100

    updated script

--- contracts/ISwapper.sol
+++ contracts/ISwapper.sol
@@ -1,15 +1,19 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-import "./price.sol";
+//import "./price.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 interface IERC20{
     function transferFrom(address _from,address _to,uint256 _amount) external returns(bool);
     function transfer(address _to,uint256 _amount) external returns(bool);
+    function approve(address spender, uint256 amount) external returns (bool);
 }
 
 contract Market{
 
+    AggregatorV3Interface internal priceFeed;
     struct Order{
         address fromToken; // Contract address usdt
         //uint88 expiry;
@@ -23,16 +27,34 @@ contract Market{
 
     uint orderIndex = 1;
 
-    PriceConsumerV3 p = new PriceConsumerV3();
-    int price = p.getLatestPriceEthUsd();
-    int rate = price / decimal;
-    int decimal;
+    // PriceConsumerV3 p = new PriceConsumerV3();
+    // int price =4;
+    // int decimal;
+    int rate;
     mapping(uint=>Order) public orders;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c);
+        //priceFeed2 = AggregatorV3Interface(0x0bF499444525a23E7Bb61997539725cA2e928138);
+    }
+
+    function getLatestPriceEthUsd() public view returns (int) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
 
     function addOrder(address _fromToken,address _toToken,uint _amountIn, int _decimal) external{
         require(IERC20(_fromToken).transferFrom(msg.sender,address(this),_amountIn),"Not accessible");
         Order storage o=orders[orderIndex];
-        decimal = _decimal;
+        int _price = getLatestPriceEthUsd();
+        int _rate = _price / _decimal;
+        rate = _rate;
         o.fromToken = _fromToken;
         o.toToken = _toToken;
         o.toTokenAmount = uint(rate);
@@ -49,9 +71,9 @@ contract Market{
         Order storage o=orders[_index];
         require(!o.done, "oder has been executed");
         assert(o.toToken != address(0));
-        uint calcRate = _amountIn/uint(rate);
-        require(IERC20(o.toToken).transferFrom(msg.sender, o.owner, _amountIn),"");
-        require(IERC20(o.fromToken).transfer(msg.sender, calcRate));
+        uint calcRate = _amountIn /uint(rate);
+        require(IERC20(o.toToken).transferFrom(msg.sender, o.owner, _amountIn),"Money not sent");
+        require(IERC20(o.fromToken).transfer(msg.sender, calcRate), "Money not recived");
 
         o.amountAvailable -= calcRate;
         o.done=o.amountAvailable==0?true:false;
--- scripts/marketScript.ts
+++ scripts/marketScript.ts
@@ -1,35 +1,67 @@
-import { Provider } from "@ethersproject/abstract-provider";
 import { providers, Signer } from "ethers";
 import { ethers } from "hardhat";
 import { IERC20 } from "../typechain/IERC20";
 
-const usdToken = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-const usdHolder = "0xbdc35c5b1042738eb01b57f8cc8f18d190a23a9c"
+const usdToken = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+const usdHolder = "0x8e99C0a22c69FC37373478f597464a87c456bCE1"
 const linkToken = "0x514910771AF9Ca656af840dff83E8264EcF986CA"
 const linkHolder = "0xc31a1ae79181bc2a3293b01cd1f23eac1a75945e"
 
 async function market(){
     const usdContract = await ethers.getContractAt("IERC20", usdToken)
     const linkContract = await ethers.getContractAt("IERC20", linkToken)
-    const swapper = await ethers.getContractAt("Market", "0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c")
+    // const price = await ethers.getContractFactory("PriceConsumerV3")
+
+    // const priceContract = await price.deploy()
+    // await priceContract.deployed()
     
-    const setOrder = await swapper.addOrder(linkToken,usdToken,1000,8)
+    const swapper = await ethers.getContractFactory("Market")
+    const swapperContract = await swapper.deploy()
+    await swapperContract.deployed()
 
-    // //Account Impersonation()
-    // // @ts-ignore
-    // await hre.network.provider.request({
-    //     method: "hardhat_impersonateAccount",
-    //     params: [linkHolder],
-    // });
+    
 
-    // const signer: Signer = await ethers.getSigner(linkHolder)
-    // const status = await IERC20.connect(signer).transfer("0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199", "10")
+     //Account Impersonation()
+     // @ts-ignore
+    await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+         params: [linkHolder],
+     });
+    const sign = await linkContract.approve(swapperContract.address,"1000000")
+    const signer: Signer = await ethers.getSigner(linkHolder)
+    // await linkContract.connect(signer).approve(swapperContract.address, "100000")
+
+    // const getsigner = await ethers.getSigners()
+    // const sender = await getsigner[0].address;
+    // const msg = await ethers.getSigner(sender)
+
+    // const status = await linkContract.connect(signer).transfer(swapperContract.address, "100000")
     // console.log(status)
 
-    const executeOrder = await swapper.swapEthUsd(1,1000)
+    const setOrder = await swapperContract.connect(signer).addOrder(linkToken,usdToken,1,8)
+    
+    
 
-    const NewBal = await ethers.provider.getBalance(linkHolder)
-    console.log(NewBal);
+    console.log(`Allowing contract to spend ${setOrder}` );
+    
+    
+
+    // const setOrder = await swapper.addOrder(linkToken,usdToken,1000,8)
+
+    //Account Impersonation2()
+    // @ts-ignore
+    await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+         params: [usdHolder],
+     });
+
+    let  Signer2 = await ethers.getSigner(usdHolder)
+    await usdContract.connect(Signer2).approve(linkHolder, "1")
+
+    await swapperContract.connect(Signer2).swapEthUsd(1,"1")
+
+    // const NewBal = await ethers.provider.getBalance(linkHolder)
+    // console.log(NewBal);
 
 }
 
